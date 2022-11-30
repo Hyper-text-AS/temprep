d13b4fcf5383536cfd8b08002fd0322620b03f90
Hyper-text-AS <119379672+Hyper-text-AS@users.noreply.github.com>
2022-11-30 10:08:14 +0800

    Multiply two numbers

---
+++ progm2.py
@@ -0,0 +1,8 @@
+#This program multiply two numbers
+def mulnum(a,b):
+  return a*b
+
+num1=int(input("Enter the num1:")
+num2=int(input("Enter the num1:")
+a=mulnum(num1,num2)
+print("product is :",a)    
