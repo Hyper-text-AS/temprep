29eaceced2e39a23fb89320846dabcc741d71826
Hyper-text-AS <119379672+Hyper-text-AS@users.noreply.github.com>
2022-11-30 10:06:22 +0800

    Adding two numbers

---
+++ progm1.py
@@ -0,0 +1,8 @@
+#This program adds two numbers
+def addnum(a,b):
+  return a+b
+
+num1=int(input("Enter the num1:")
+num2=int(input("Enter the num1:")
+a=addnum(num1,num2)
+print("Sum is :",a)        
