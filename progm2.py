446023f2c26b8ecb21ac099e9e3bc86eb9511056
Hyper-text-AS <119379672+Hyper-text-AS@users.noreply.github.com>
2022-11-30 10:11:47 +0800

    Reduced lines in code

--- progm2.py
+++ progm2.py
@@ -1,8 +1,7 @@
 #This program multiply two numbers
 def mulnum(a,b):
   return a*b
-
 num1=int(input("Enter the num1:")
 num2=int(input("Enter the num1:")
-a=mulnum(num1,num2)
-print("product is :",a)    
+print("The code is balanced")
+print("product is :",mulnum(num1,num2))    
